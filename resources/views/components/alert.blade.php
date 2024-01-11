5ddfdedb3e863e0e4a2cf7e10c3d5af98da9d1d6
igomis <igomis@cipfpbatoi.es>
2024-01-11 13:44:55 +0100

    alert.blade.php

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-{{ $level }}-100 border-l-4 border-{{ $level }}-500 text-{{ $level }}-700 p-4" role="alert">
-    <p class="font-bold">Atention: </p>
-    <p>{{ $message }}</p>
-    <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none';" aria-label="Close">
-        <span class="text-{{ $level }}-500">&times;</span>
-    </button>
+    <p class="font-bold">Atention: {{ $message }}
+        <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3" onclick="this.parentElement.style.display='none';" aria-label="Close">
+            <span class="text-{{ $level }}-500">&times;</span>
+        </button>
+    </p>
 </div>
