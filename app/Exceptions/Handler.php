70d0eaaab6151794242bd7b0d234eed191d10289
igomis <igomis@cipfpbatoi.es>
2024-01-15 17:38:11 +0100

    alert.blade.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +30,30 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+        $this->renderable(function (Throwable $exception) {
+
+            if (request()->is('api*'))
+            {
+                if ($exception instanceof ModelNotFoundException) {
+                    return response()->json(['error' => 'Elemento no encontrado'], 404);
+                }
+                elseif ($exception instanceof AuthenticationException) {
+                    return response()->json(['error' => 'Usuario no autenticado'], 401);
+                }
+                elseif ($exception instanceof ValidationException) {
+                    return response()->json([
+                        'error' => 'Datos no válidos',
+                        'errors' => $exception->validator->getMessageBag(),
+                    ], 400);
+                }
+                elseif ($exception instanceof QueryException) {
+                    return response()->json(['error' => 'Datos no válidos'], 400);
+                }
+                elseif (isset($exception)) {
+                    return response()->json(['error' => 'Error en la aplicación ('.get_class($exception).'):'.$exception->getMessage()], 500);
+                }
+            }
+        });
+
     }
 }
--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookRequest;
 use App\Http\Resources\BookCollection;
 use App\Http\Resources\BookResource;
 use App\Models\Book;
+use App\Notifications\BookNewNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -22,9 +25,19 @@ class BookController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BookRequest $request)
     {
-        //
+        $validatedData = $request->toArray();
+
+        if ($request->hasFile('photo')) {
+            $path = $request->file('photo')->store('public/books');
+            $validatedData['photo'] = Storage::url($path);
+        }
+
+        $book = Book::create($validatedData);
+        administrador()->notify(new BookNewNotification($book));
+
+        return response()->json(new BookResource($book),200);
     }
 
     /**
